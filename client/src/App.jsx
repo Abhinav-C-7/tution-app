07b9fba985c9e3f080ac059affa1f904e036f864
Abhinav-C-7 <abhinavc038@gmail.com>
2025-12-29 14:17:35 +0530

    Added BatchCard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-
-
+import BatchCard from "./components/BatchCard"
 function App() {
   
 
   return (
     <>
+<BatchCard></BatchCard>
       <h1>myTution</h1>
     </>
   )
---
+++ client/src/components/BatchCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BatchCard = () => {
+  return (
+    <div className='w-56 h-56 bg-blue-100'>
+      
+    </div>
+  )
+}
+
+export default BatchCard
