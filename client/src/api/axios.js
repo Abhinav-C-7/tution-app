a8ec5d8038e25b6c1e5a2b75c8b45d7b140ac860
Abhinav-C-7 <abhinavc038@gmail.com>
2026-01-18 18:20:31 +0530

    Corrected backend api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://tution-app-4esn.onrender.com';
+const BASE_URL = 'https://tution-app-4esn.onrender.com/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
