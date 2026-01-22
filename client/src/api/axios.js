d9a7daa19d9fa7a9a1b0bb5f11f41a2847e579e7
Abhinav-C-7 <abhinavc038@gmail.com>
2026-01-22 17:47:33 +0530

    API switched to live backend api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // ❌ Comment out the Live URL when testing locally
-// const BASE_URL = 'https://tution-app-4esn.onrender.com/api';
+const BASE_URL = 'https://tution-app-4esn.onrender.com/api';
 
 // ✅ Uncomment this for Local Development
-const BASE_URL = 'http://localhost:5000/api'; // Make sure this matches your Backend PORT!
+// const BASE_URL = 'http://localhost:5000/api'; // Make sure this matches your Backend PORT!
 
 const api = axios.create({
     baseURL: BASE_URL,
