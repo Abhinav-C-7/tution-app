92883652e4b6f57ba36a4e650f1623e530f25b8c
Abhinav-C-7 <abhinavc038@gmail.com>
2026-02-03 22:58:23 +0530

    update: CORS policy

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://coursecore.online", "https://www.coursecore.online"],
+    credentials: true
+}));
 
 /**
  * CRITICAL: Webhook route must use express.raw()
